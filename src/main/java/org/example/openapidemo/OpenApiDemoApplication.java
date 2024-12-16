df5b399de9fb137d0f45946ea9cedfa9129fdae9
Jia Ding <jia.ding@prodyna.com>
2024-12-16 14:49:01 +0100

    first commit

---
+++ src/main/java/org/example/openapidemo/OpenApiDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.openapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiDemoApplication.class, args);
+    }
+
+}
